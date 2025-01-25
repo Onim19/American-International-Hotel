16b2823cec4d8aed20f22b89a30eef5e7b238001
Onim Hassan <168928238+Onim19@users.noreply.github.com>
2025-01-25 14:55:46 +0600

    Add files via upload

---
+++ src/Hotel_Management_System/Person/Details/StaffDetails.java
@@ -0,0 +1,22 @@
+package Hotel_Management_System.Person.Details;
+
+public class StaffDetails {
+    private String id,name,age,gender,job,phone,salary;
+    public StaffDetails(String id,String name,String age,String gender,String job,String salary,String phone) {
+        this.id=id;
+        this.name = name;
+        this.age=age;
+        this.gender=gender;
+        this.job=job;
+        this.salary=salary;
+        this.phone=phone;
+    }
+    //adding getter methods for private attributes
+    public String getId(){return this.id;}
+    public String getName(){return this.name;}
+    public String getAge(){return this.age;}
+    public String getGender(){return this.gender;}
+    public String getJob(){return this.job;}
+    public String getSalary(){return this.salary;}
+    public String getPhone(){return this.phone;}
+}
