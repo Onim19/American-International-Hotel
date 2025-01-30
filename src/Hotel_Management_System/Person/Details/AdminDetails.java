edb0489ae931c4918268c7988f632df7e1f55628
Onim Hassan <168928238+Onim19@users.noreply.github.com>
2025-01-30 13:55:05 +0600

    Update AdminDetails.java

--- src/Hotel_Management_System/Person/Details/AdminDetails.java
+++ src/Hotel_Management_System/Person/Details/AdminDetails.java
@@ -1,11 +1,15 @@
 package Hotel_Management_System.Person.Details;
 
 public class AdminDetails {
-    String username,password;
-    public AdminDetails(String username,String password){
+    private String username,email,phone,password;
+    public AdminDetails(String username,String email,String phone,String password){
         this.username=username;
+        this.email=email;
+        this.phone=phone;
         this.password=password;
     }
     public String getUsername(){return this.username;}
+    public String getEmail(){return this.email;}
+    public String getPhone(){return this.phone;}
     public String getPassword(){return this.password;}
 }
