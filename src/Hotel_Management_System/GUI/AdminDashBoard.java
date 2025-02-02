26a75ee7f61c12295d3202c3f636fb45e39a8ac2
Onim Hassan <168928238+Onim19@users.noreply.github.com>
2025-02-02 23:28:45 +0600

    Update AdminDashBoard.java

--- src/Hotel_Management_System/GUI/AdminDashBoard.java
+++ src/Hotel_Management_System/GUI/AdminDashBoard.java
@@ -1,136 +1,154 @@
 package Hotel_Management_System.GUI;
+
 import Hotel_Management_System.Person.*;
+import Hotel_Management_System.Room.Room;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.*;
 
-public class Admin extends JFrame implements ActionListener{
-    JTextField userName;
-    JPasswordField passWord;
-    JButton Login,Cancel;
+public class AdminDashBoard extends JFrame implements ActionListener {
+    JButton btnEmployee,btnRoom,btnStaff,btnGuest,LogOut,Register_Admin;
 
-    public Admin() {
+    public AdminDashBoard() {
         super.getContentPane().setBackground(Color.LIGHT_GRAY);
-        super.setSize(1280,720);
-        super.setLocation(150,50);
+        super.setSize(1280, 720);
+        super.setLocation(150, 50);
         super.setLayout(null);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setResizable(false);
-        ImageIcon icon=new ImageIcon("src/img/Icon.png");//Icon
-        ImageIcon login_bg1=new ImageIcon("src/img/Login_bg.png");
-        super.setIconImage(icon.getImage());//adding icon
-        super.setTitle("Log in");
+        ImageIcon icon=new ImageIcon("src/img/Icon.png"); //Icon
+        super.setIconImage(icon.getImage()); //Adding icon
+        super.setTitle("Admin Dashboard");
 
-        //pre-sizing the image
-        Image login_bg2=login_bg1.getImage().getScaledInstance(1280,720,Image.SCALE_DEFAULT);
-        ImageIcon login_bg=new ImageIcon(login_bg2);
+        // Adding Background Image
+        ImageIcon image1=new ImageIcon("src/img/AdminDashboard.jpg");
+        Image image2=image1.getImage().getScaledInstance(1280,720,Image.SCALE_DEFAULT);
+        ImageIcon image3=new ImageIcon(image2);
 
-        //adding a label for adding background image
-        JLabel l_bg=new JLabel(login_bg);
-        l_bg.setBounds(0,0,1280,720);
-        super.add(l_bg);
+        JLabel image=new JLabel(image3);
+        image.setBounds(0,0,1280,720);
+        super.add(image);
 
-        //adding a picture
-        ImageIcon picture1=new ImageIcon("src/img/Login.png");
-        Image picture2=picture1.getImage().getScaledInstance(610,418,Image.SCALE_DEFAULT);
-        ImageIcon picture3=new ImageIcon(picture2);
-        JLabel picture=new JLabel(picture3);
-        picture.setBounds(700,150,610,418);
-        l_bg.add(picture);
-        //adding label for user name
-        JLabel user_name=new JLabel("Username : ");
-        user_name.setBounds(200,250,100,30);//adjusted position
-        user_name.setForeground(Color.WHITE);//set label color to white
-        l_bg.add(user_name);
+        // Common font for button labels
+        Font buttonFont=new Font("Arial", Font.BOLD, 14);
 
-        //adding textbox for username
-        userName=new JTextField();
-        userName.setBounds(325,250,250,30);
-        l_bg.add(userName);
+        // Adding Employee button with label
+        ImageIcon employee1=new ImageIcon("src/img/Employee.png");
+        Image employee2=employee1.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        ImageIcon employee3=new ImageIcon(employee2);
+        btnEmployee=new JButton("Employee", employee3);
+        btnEmployee.setVerticalTextPosition(SwingConstants.BOTTOM);
+        btnEmployee.setHorizontalTextPosition(SwingConstants.CENTER);
+        btnEmployee.setBounds(20, 500, 140, 160);
+        btnEmployee.setFocusable(false);
+        btnEmployee.setBackground(Color.BLACK);
+        btnEmployee.setForeground(Color.WHITE);
+        btnEmployee.setFont(buttonFont);
+        btnEmployee.addActionListener(this);
+        image.add(btnEmployee);
 
-        //adding label for password
-        JLabel password=new JLabel("Password  : ");
-        password.setBounds(200,300,100,25);//adjusted position
-        password.setForeground(Color.WHITE);//set label color to white
-        l_bg.add(password);
+        // Adding Room button with label
+        ImageIcon room1=new ImageIcon("src/img/Room.png");
+        Image room2=room1.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        ImageIcon room3=new ImageIcon(room2);
+        btnRoom=new JButton("Room", room3);
+        btnRoom.setVerticalTextPosition(SwingConstants.BOTTOM);
+        btnRoom.setHorizontalTextPosition(SwingConstants.CENTER);
+        btnRoom.setBounds(180, 500, 140, 160);
+        btnRoom.setFocusable(false);
+        btnRoom.setBackground(Color.BLACK);
+        btnRoom.setForeground(Color.WHITE);
+        btnRoom.setFont(buttonFont);
+        btnRoom.addActionListener(this);
+        image.add(btnRoom);
 
-        //adding textbox for password
-        passWord=new JPasswordField();
-        passWord.setBounds(325,300,250,25);
-        l_bg.add(passWord);
+        // Adding Staff button with label
+        ImageIcon staff1=new ImageIcon("src/img/Staff.png");
+        Image staff2=staff1.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        ImageIcon staff3=new ImageIcon(staff2);
+        btnStaff=new JButton("Staff", staff3);
+        btnStaff.setVerticalTextPosition(SwingConstants.BOTTOM);
+        btnStaff.setHorizontalTextPosition(SwingConstants.CENTER);
+        btnStaff.setBounds(500, 500, 140, 160);
+        btnStaff.setFocusable(false);
+        btnStaff.setBackground(Color.BLACK);
+        btnStaff.setForeground(Color.WHITE);
+        btnStaff.setFont(buttonFont);
+        btnStaff.addActionListener(this);
+        image.add(btnStaff);
 
-        //login button
-        Login=new JButton("Log in");
-        Login.setBounds(360,350,70,30);
-        Login.setFocusable(false);
-        Login.setBackground(Color.WHITE);//set background color to white
-        Login.setForeground(Color.BLACK);//set font color to black
-        Login.addActionListener(this);
-        Login.setFont(new Font("Serif",Font.BOLD,12));
-        l_bg.add(Login);
+        // Adding Guest button with label
+        ImageIcon guest1=new ImageIcon("src/img/Guest.png");
+        Image guest2=guest1.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH);
+        ImageIcon guest3=new ImageIcon(guest2);
+        btnGuest=new JButton("Guest", guest3);
+        btnGuest.setVerticalTextPosition(SwingConstants.BOTTOM);
+        btnGuest.setHorizontalTextPosition(SwingConstants.CENTER);
+        btnGuest.setBounds(340, 500, 140, 160);
+        btnGuest.setFocusable(false);
+        btnGuest.setBackground(Color.BLACK);
+        btnGuest.setForeground(Color.WHITE);
+        btnGuest.setFont(buttonFont);
+        btnGuest.addActionListener(this);
+        image.add(btnGuest);
 
-        //cancel button
-        Cancel=new JButton("Cancel");
-        Cancel.setBounds(460,350,70,30);
-        Cancel.setFocusable(false);
-        Cancel.setBackground(Color.WHITE);//set background color to white
-        Cancel.setForeground(Color.BLACK);//set font color to black
-        Cancel.addActionListener(this);
-        Cancel.setFont(new Font("Serif",Font.BOLD,12));
-        l_bg.add(Cancel);
+        //Adding a logout button
+        LogOut=new JButton("Log out");
+        LogOut.setBounds(1050,600,150,50);
+        LogOut.setFocusable(false);
+        LogOut.setBackground(Color.RED);
+        LogOut.setFont(new Font("Arial", Font.BOLD, 22));
+        LogOut.setForeground(Color.WHITE);
+        LogOut.addActionListener(this);
+        image.add(LogOut);
 
+        //register button
+        Register_Admin=new JButton("Register admin");
+        Register_Admin.setBounds(880,600,150,50);
+        Register_Admin.setFocusable(false);
+        Register_Admin.setBackground(Color.WHITE);//set background color to white
+        Register_Admin.setForeground(Color.BLACK);//set font color to black
+        Register_Admin.setFont(new Font("Arial",Font.BOLD,15));
+        Register_Admin.addActionListener(this);
+        image.add(Register_Admin);
 
         super.setVisible(true);
     }
 
+    @Override
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource()==Login) {
-            admin();
-        } else if(e.getSource()==Cancel) {
-            super.dispose();
-            new Welcome();
-        }
-    }
-
-    public void admin(){
-        String username=userName.getText();
-        String password=new String(passWord.getPassword());
-        try {
-            File file=new File("src/Hotel_Management_System/Person/Details/TextFiles/AdminDetails.txt");
-            if(!file.exists()){
-                JOptionPane.showMessageDialog(null,"No user is registered!");
-                return;
-            }
-            BufferedReader reader=new BufferedReader(new FileReader(file));
-            String line;
-            boolean loggedin=false;
-            while((line=reader.readLine())!=null){
-                String[] parts=line.split(",");
-                if((parts[0].equals(username)||parts[1].equals(username)||parts[2].equals(username))&&parts[3].equals(password)){
-                    loggedin=true;
-                    break;
-                }
-            }
-            reader.close();
-            if(loggedin){
-                JOptionPane.showMessageDialog(this,"Login successful");
+        if(e.getSource()==LogOut){
+            int response=JOptionPane.showConfirmDialog(this,"Logout?","Confirm Log Out",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+            if(response==JOptionPane.YES_OPTION){
                 super.dispose();
-                new AdminDashBoard();
-            }
-            else {
-                JOptionPane.showMessageDialog(this,"Invalid username or password");
-                return;
+                new Welcome();
             }
         }
-        catch(IOException ae){
-            ae.printStackTrace();
+        else if(e.getSource()==btnEmployee) {
+            super.dispose();
+            new Employee();
+        }
+        else if(e.getSource()==btnStaff) {
+            super.dispose();
+            new Staff();
+        }
+        else if(e.getSource()==btnRoom){
+            super.dispose();
+            new Room();
+        }
+        else if(e.getSource()==btnGuest){
+            super.dispose();
+            new Guest();
+        }
+        else if(e.getSource()==Register_Admin){
+            super.dispose();
+            new RegisterAdmin();
         }
     }
 
     public static void main(String[] args) {
-        new Admin();
+        new AdminDashBoard();
     }
 }
