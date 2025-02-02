99b0fa5a27e0e10575741efcd59a305c3d65acf8
Onim Hassan <168928238+Onim19@users.noreply.github.com>
2025-02-03 01:02:16 +0600

    Update Room.java

--- src/Hotel_Management_System/Room/Room.java
+++ src/Hotel_Management_System/Room/Room.java
@@ -1,18 +1,18 @@
 package Hotel_Management_System.Room;
 
-//import Hotel_Management_System.Room.RoomDetails.PremiumRoom;
-
+import Hotel_Management_System.GUI.AdminDashBoard;
+import Hotel_Management_System.Room.RoomDetails.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Room extends JFrame implements ActionListener {
-    JButton btnpremium, btnvip, btngeneral;
+    JButton btnpremium,btnvip,btngeneral,back;
 
     public Room() {
         super.getContentPane().setBackground(Color.LIGHT_GRAY);
-        super.setSize(720, 540);
+        super.setSize(720, 580);
         super.setLocationRelativeTo(null); // Centers the window
         super.setLayout(null);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -20,6 +20,15 @@ public class Room extends JFrame implements ActionListener {
         ImageIcon icon = new ImageIcon("src/img/Icon.png"); // Icon
         super.setIconImage(icon.getImage()); // Adding icon
         super.setTitle("Room Details");
+
+        // Adding Background Image
+        ImageIcon image1=new ImageIcon("src/img/Room.jpg");
+        Image image2=image1.getImage().getScaledInstance(720,580,Image.SCALE_DEFAULT);
+        ImageIcon image3=new ImageIcon(image2);
+
+        JLabel image=new JLabel(image3);
+        image.setBounds(0,0,720,580);
+        super.add(image);
 
         int buttonWidth = 200;
         int buttonHeight = 40;
@@ -29,37 +38,47 @@ public class Room extends JFrame implements ActionListener {
         btnpremium.setBounds(centerX, 260,buttonWidth,buttonHeight);
         btnpremium.setFocusable(false);
         btnpremium.addActionListener(this);
-        super.add(btnpremium);
+        image.add(btnpremium);
 
         btnvip = new JButton("VIP Rooms");
         btnvip.setBounds(centerX, 200,buttonWidth,buttonHeight);
         btnvip.setFocusable(false);
-        btngeneral.addActionListener(this);
-        super.add(btnvip);
+        btnvip.addActionListener(this);
+        image.add(btnvip);
 
         btngeneral = new JButton("General Rooms");
         btngeneral.setBounds(centerX, 320,buttonWidth,buttonHeight);
         btngeneral.setFocusable(false);
         btngeneral.addActionListener(this);
-        super.add(btngeneral);
+        image.add(btngeneral);
+
+        back = new JButton("Back");
+        back.setBounds(310, 380,100,buttonHeight);
+        back.setFocusable(false);
+        back.addActionListener(this);
+        image.add(back);
+
 
         super.setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-       /* if(e.getSource()==btnvip){
-            new PremiumRoom(false);
-        }
+       if(e.getSource()==btnvip){
+           super.dispose();
+           new VipRoom();
+       }
        else if(e.getSource()==btnpremium){
-            new PremiumRoom();
-        }
-        else if(e.getSource()==btngeneral){
-            new GeneralRoom();
-        } */
-    }
-
-    public static void main(String[] args) {
-        new Room();
+           super.dispose();
+           new PremiumRoom();
+       }
+       else if(e.getSource()==btngeneral){
+           super.dispose();
+           new GeneralRoom();
+       }
+       else if(e.getSource()==back){
+           super.dispose();
+           new AdminDashBoard();
+       }
     }
 }
